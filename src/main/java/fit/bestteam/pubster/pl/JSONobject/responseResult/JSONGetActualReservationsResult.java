bd127197fa5d75767ea3f7cf0177dc0e383bc1b3
illia <illia@illia-HOME>
2019-05-07 21:37:51 +0200

    Added JSON classes for Jackson usage (Method results)

---
+++ src/main/java/fit/bestteam/pubster/pl/JSONobject/responseResult/JSONGetActualReservationsResult.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fit.bestteam.pubster.pl.JSONobject.responseResult;
+
+import fit.bestteam.pubster.pl.JSONobject.common.reservation.JSONReservation;
+import java.util.List;
+
+/**
+ *
+ * @author illia
+ */
+public class JSONGetActualReservationsResult {
+    
+    private List<JSONReservation> list;
+
+    public JSONGetActualReservationsResult() {
+    }
+
+    public JSONGetActualReservationsResult(List<JSONReservation> list) {
+        this.list = list;
+    }
+
+    public List<JSONReservation> getList() {
+        return list;
+    }
+
+    public void setList(List<JSONReservation> list) {
+        this.list = list;
+    }
+    
+}
